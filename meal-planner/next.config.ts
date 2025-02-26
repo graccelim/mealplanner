32bd5a4c42914a23405b3616c847ebecb326192e
graccelim <graccelim@gmail.com>
2025-02-26 09:23:36 +0800

    Update next.config.ts

--- meal-planner/next.config.ts
+++ meal-planner/next.config.ts
@@ -15,8 +15,29 @@ const nextConfig = {
     // Disable TypeScript checks during builds
     ignoreBuildErrors: true,
   },
+  reactStrictMode: true,
+  experimental: {
+    runtime: "edge",
+  },
 }
 
 module.exports = nextConfig
+
+
+
+
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     // Disable ESLint during builds
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     // Disable TypeScript checks during builds
+//     ignoreBuildErrors: true,
+//   },
+// }
+
+// module.exports = nextConfig
 
 
